4562b2cfc092631803c8fdfb8ac2efb08cb839c8
Dom Bryan <dombguitar@gmail.com>
2022-03-25 10:37:43 +0000

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Dom Bryan on 25/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
